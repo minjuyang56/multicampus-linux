715d376c8ff67b01dcd9a25d6f11306b6332a771
alswn0558@gmail.com <alswn0558@gmail.com>
2024-11-12 08:05:53 +0000

    - rocks are scattered.

--- practice/find_rocks/rocks/hide_rocks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mv rock1 $(find ../4 -type d | shuf -n 1)
-mv rock2 $(find ../4 -type d | shuf -n 1)
-mv rock3 $(find ../4 -type d | shuf -n 1)
-mv rock4 $(find ../4 -type d | shuf -n 1)
-mv rock5 $(find ../4 -type d | shuf -n 1)
